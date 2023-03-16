c84ca77220d76e05fa4fbbbb96821a8f0c939929
GuiMartos <guilhermemartos2005@gmail.com>
2023-03-16 08:14:19 -0300

    Começando o "ts"

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,25 @@ export class Tab2Page {
 
   constructor() {}
 
+  numero= 1;
+
+  mudarNumero(){
+    this.numero=1
+  }
+
+  mudarNumero3(){
+    this.numero=3
+  }
+
+  mudarNumero6(){
+    this.numero=6
+  }
+
+  mudarNumero9(){
+    this.numero=9
+  }
+
+  mudarNumero12(){
+    this.numero=12
+  }
 }
